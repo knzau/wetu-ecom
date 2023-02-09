7d1fbe00b6dd889c39ff198890e1e2daf1826c53
kennedy nzau <kenn.nzau@gmail.com>
2023-02-08 22:51:32 -0300

    use clean api query for producttype endpoints

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import Product from '../Product/Product';
 import './ProductList.scss';
 
-const ProductList = ({ productsData }) => {
+const ProductList = ({ productsData, categoryTitle = '' }) => {
   return (
     <div className="product-list__wrapper">
-      {productsData.map((product) => (
-        <Product
-          product={product}
-          key={product.id}
-          categoryTitle={product?.attributes?.categories?.data[0]?.attributes?.title}
-        />
-      ))}
+      {productsData
+        ? productsData.map((product) => (
+            <Product
+              product={product}
+              key={product.id}
+              categoryTitle={
+                categoryTitle
+                  ? categoryTitle
+                  : product?.attributes?.categories?.data[0]?.attributes?.title
+              }
+            />
+          ))
+        : ''}
     </div>
   );
 };
--- client/src/components/ProductType/ProductType.jsx
+++ client/src/components/ProductType/ProductType.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import TabPane from '../TabsComponent/TabPane';
-import Product from '../Product/Product';
 import useFetch from '../../hooks/useFetch';
+import ProductList from '../ProductList/ProductList';
+import qs from 'qs';
 
 const ProductType = ({ productType, categoryId, categoryTitle }) => {
-  const { data, loading } = useFetch(
-    `/products?populate=*&[filters][categories][id]=${categoryId}&[filters][type][$eq]=${productType.label}`
+  const query = useMemo(
+    () =>
+      qs.stringify(
+        {
+          filters: {
+            categories: { id: categoryId },
+            type: {
+              $eq: productType.label
+            }
+          },
+          pagination: {
+            pageSize: 8
+          }
+        },
+        {
+          encodeValuesOnly: true // prettify URL
+        }
+      ),
+    [categoryId, productType.label]
   );
+
+  const { data, loading } = useFetch(`/products?populate=*&${query}`, [
+    categoryId,
+    productType.label
+  ]);
 
   return (
     <TabPane title={productType.label} productType={productType.label} key={productType.id}>
       <div className="products_container">
-        {loading
-          ? 'loading'
-          : data?.map((product) => (
-              <Product key={product.id} product={product} categoryTitle={categoryTitle} />
-            ))}
+        {loading ? 'loading' : <ProductList productsData={data} categoryTitle={categoryTitle} />}
       </div>
     </TabPane>
   );
