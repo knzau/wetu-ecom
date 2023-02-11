31c54a58e25e631e6ff917eba11d0fe973c4eaec
kennedy nzau <kenn.nzau@gmail.com>
2023-02-10 23:15:26 -0300

    refactor handle filters function

--- client/src/pages/Categories/Categories.jsx
+++ client/src/pages/Categories/Categories.jsx
@@ -5,11 +5,19 @@ import ProductList from '../../components/ProductList/ProductList';
 import BreadCrumb from '../../components/BreadCrumb/BreadCrumb';
 import useFetch from '../../hooks/useFetch';
 import qs from 'qs';
+import { brandFilters, getFilterString, sizeFilters } from '../../utils';
 import './Categories.scss';
 
 const Categories = () => {
   const categoryId = useParams();
   const [priceRangeFilter, setPriceRangeFilter] = useState('7-1200');
+  const [checkedSizeFilter, setCheckedSizeFilter] = useState(
+    new Array(sizeFilters.length).fill(false)
+  );
+  const [checkedBrandFilter, setBrandCheckedFilter] = useState(
+    new Array(brandFilters.length).fill(false)
+  );
+  const [filter, setFilter] = useState({});
 
   const [startPrice, endPrice] = useMemo(() => {
     return priceRangeFilter === '1200+' ? ['1200', '15000'] : priceRangeFilter.split('-');
@@ -19,28 +27,49 @@ const Categories = () => {
     () =>
       qs.stringify(
         {
+          populate: '*',
           filters: {
             categories: categoryId,
             price: {
               $gte: startPrice,
               $lte: endPrice
-            }
+            },
+            ...filter
           }
         },
         {
           encodeValuesOnly: true // prettify URL
         }
       ),
-    [startPrice, endPrice, categoryId]
+    [startPrice, endPrice, categoryId, filter]
   );
 
   const handlePriceRangeSelect = (e) => {
     setPriceRangeFilter(e.target.value);
   };
 
-  const { data, loading } = useFetch(`/products?populate=*&${query}`, [categoryId.id, query]);
+  const { data, loading } = useFetch(`/products?${query}`, [categoryId.id, query]);
 
   const title = data ? data[0]?.attributes?.categories?.data[0]?.attributes?.title : '';
+
+  const handleClickFilters = (filter, filterId, position) => {
+    const updatedCheckedState = checkedSizeFilter.map((item, index) =>
+      index === position ? !item : item
+    );
+    if (filterId === 'color') {
+      console.log({ filter, filterId });
+      setFilter({ [filterId]: filter });
+    }
+    if (filterId === 'size') {
+      setCheckedSizeFilter(updatedCheckedState);
+
+      setFilter({ [filterId]: { $contains: getFilterString(updatedCheckedState, sizeFilters) } });
+    }
+    if (filterId === 'brand') {
+      setFilter({ [filterId]: { $contains: getFilterString(updatedCheckedState, brandFilters) } });
+      setBrandCheckedFilter(updatedCheckedState);
+    }
+  };
 
   return (
     <div className="category-page__container">
@@ -49,7 +78,12 @@ const Categories = () => {
         <span className="page-banner__title">{title}</span>
       </div>
       <div className="page__content">
-        <Filters handlePriceRangeSelect={handlePriceRangeSelect} />
+        <Filters
+          handlePriceRangeSelect={handlePriceRangeSelect}
+          handleClickFilters={handleClickFilters}
+          checkedSizeFilter={checkedSizeFilter}
+          checkedBrandFilter={checkedBrandFilter}
+        />
         {loading ? 'loading' : data && <ProductList productsData={data} />}
       </div>
     </div>
