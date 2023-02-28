fd8dbbeb2974bc9a01776c63ce04f8469099fd20
kennedy nzau <kenn.nzau@gmail.com>
2023-02-27 21:24:24 -0300

    update api config

--- api/config/database.js
+++ api/config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5433),
-      database: env('DATABASE_NAME', 'store'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '12345678'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
     },
   },
 });
